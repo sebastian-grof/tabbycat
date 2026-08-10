d973f0c39b877e2c4488c41adacbd01c7d22675b
Sebastian Grof <sebastiangrof25@gmail.com>
2026-08-10 15:24:53 +0200

    Repair migration graph and adapt upstream code to fork criteria API

---
+++ tabbycat/tournaments/migrations/0019_merge_20260810.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("tournaments", "0016_round_schedule_group"),
+        ("tournaments", "0018_tournamentcategory_parent"),
+    ]
+
+    operations = []
